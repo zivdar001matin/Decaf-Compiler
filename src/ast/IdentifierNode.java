1a027a71f0a996c004393a1e160d195e8f5eeb22
Negaar <zivdar1matin@gmail.com>
2021-01-22 03:23:45 +0330

    Initialize IdentifierNode.

---
+++ src/ast/IdentifierNode.java
@@ -0,0 +1,28 @@
+package ast;
+
+public class IdentifierNode extends SimpleNode{
+    private final String value;
+    private boolean inConst;
+
+    public IdentifierNode(String value) {
+        super(NodeType.IDENTIFIER);
+        this.value = value;
+    }
+
+    public boolean isInConst() {
+        return inConst;
+    }
+
+    public void setInConst(boolean inConst) {
+        this.inConst = inConst;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return "%"+value;
+    }
+}
