625e2188f9ba48bab60f49d860464ff5861c444f
Matin <zivdar1matin@gmail.com>
2021-01-26 18:59:04 +0330

    add methods to VTable

--- src/codegen/vtable/VTable.java
+++ src/codegen/vtable/VTable.java
@@ -3,9 +3,21 @@ package codegen.vtable;
 import java.util.HashMap;
 
 public class VTable {
-    private final HashMap<String, CodeForFunct> scope;
+    private final HashMap<String, CodeForFunct> table;
 
     public VTable() {
-        this.scope = new HashMap<>();
+        this.table = new HashMap<>();
+    }
+
+    public void addFunction(String name, CodeForFunct codeForFunct){
+        table.put(name, codeForFunct);
+    }
+
+    public CodeForFunct getFunction(String name) throws Exception {
+        CodeForFunct code = table.get(name);
+        if (code == null) {
+            throw new Exception("Function " + name + " not defined!");
+        }
+        return code;
     }
 }
