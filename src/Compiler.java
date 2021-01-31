c609604e3d49e249dd68d4c31fb10c9198b64a40
Matin <zivdar1matin@gmail.com>
2021-01-31 11:03:36 +0330

    Cleanup Compiler.java

--- src/Compiler.java
+++ src/Compiler.java
@@ -1,6 +1,4 @@
 import codegen.CodeGen;
-import codegen.SemanticError;
-import codegen.ast.Node;
 import parser.*;
 import scanner.*;
 
@@ -11,31 +9,14 @@ public class Compiler {
         FileReader fr = new FileReader("tests/test.d");
         Scanner scanner = new Scanner(fr);
         parser parser = new parser(scanner);
-        boolean isSemanticError = false;
+
         try {
             parser.parse();
-            CodeGen.cgen(parser.getRoot());
-            CodeGen.compile();
-            System.out.println("####################### asm code ######################");
-            System.out.print(CodeGen.getDataSeg());
-            System.out.print(CodeGen.getTextSeg());
-            System.out.println("#######################################################");
-        }catch (SemanticError e){
-            isSemanticError = true;
-            throw e;
-        }catch (Exception e){
-            if (isSemanticError)
-                throw e;
+        } catch (Exception e){
             throw new SyntaxError(e.getMessage());
         }
-    }
 
-    private static void printTree(Node node) {
-        for (Node child : node.getChildren()) {
-            System.out.println(child + " ---------> " + node);
-        }
-        for (Node child : node.getChildren()) {
-            printTree(child);
-        }
+        CodeGen.cgen(parser.getRoot());
+        CodeGen.compile();
     }
 }
